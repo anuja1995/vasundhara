e97ad8a760381c87f3b70bd900a4e02e3a05a308
unknown <anuja1195>
2021-07-12 17:48:53 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vasundhara_project.wsgi
--- vasundhara_project/settings.py
+++ vasundhara_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'vasundhara_project/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
