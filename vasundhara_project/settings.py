f70a5020a8049471ef04ef5516fd3ea9bdb3e74e
unknown <anuja1195>
2021-07-14 19:48:46 +0530

    edit

--- vasundhara_project/settings.py
+++ vasundhara_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'm=vytl^v#s#2i9exp3*f+=@wpyjwr&d_s=0za5=!1wzrx&oa!$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['vasundhara-project.herokuapp.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
